5678dbb342aafdb2587fa1cb3998e04b53f0e045
im genius <im.genius.man@gmail.com>
2016-06-02 16:25:15 +0500

    add comments to the listings and fix design of listing detail page

--- spec/models/featured_slider_spec.rb
+++ spec/models/featured_slider_spec.rb
@@ -2,12 +2,16 @@
 #
 # Table name: featured_sliders
 #
-#  id         :integer          not null, primary key
-#  listing_id :integer
-#  is_active  :boolean
-#  image_for  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  listing_id         :integer
+#  is_active          :boolean
+#  image_for          :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 require 'rails_helper'
